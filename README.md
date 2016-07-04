da649a47767960bd773452d547ceff99521ac2b8
Rana Das <rana.pratap.das@gmail.com>
2016-07-04 13:48:33 +0100

    added  thymelef fragments to the UI

---
+++ README.md
@@ -0,0 +1,7 @@
+some urls to keep on mind :
+
+
+1.  https://stormpath.com/blog/spring-boot_id-site_heroku
+    https://github.com/stormpath/spring-boot-idsite-sso-demo
+
+2.  https://github.com/chris-bailey/spring-boot-thymeleaf-heroku-template
--- sso-app/src/main/groovy/com/rdas/IDSiteDemoApplication.groovy
+++ sso-app/src/main/groovy/com/rdas/IDSiteDemoApplication.groovy
@@ -4,6 +4,11 @@ import org.springframework.boot.SpringApplication
 import org.springframework.boot.autoconfigure.SpringBootApplication
 
 
+/**
+ * https://github.com/chris-bailey/spring-boot-thymeleaf-heroku-template/blob/master/src/main/java/com/chrisbaileydeveloper/myapp/Application.java
+ * https://github.com/chris-bailey/spring-boot-thymeleaf-heroku-template/blob/master/src/main/java/com/chrisbaileydeveloper/myapp/ApplicationWebXml.java
+ *
+ */
 @SpringBootApplication
 public class IDSiteDemoApplication {
     public static void main(String[] args) {
