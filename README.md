c91e7ee6055fefdf5ba25735fe9c71dd443af0df
Rana Das <rana.pratap.das@gmail.com>
2016-07-04 15:46:03 +0100

    printing request headers

--- README.md
+++ README.md
@@ -4,4 +4,9 @@ some urls to keep on mind :
 1.  https://stormpath.com/blog/spring-boot_id-site_heroku
     https://github.com/stormpath/spring-boot-idsite-sso-demo
 
-2.  https://github.com/chris-bailey/spring-boot-thymeleaf-heroku-template
+2.  ssp-app based on : https://github.com/chris-bailey/spring-boot-thymeleaf-heroku-template
+
+
+
+
+3. Client-app based on : https://github.com/spring-projects/spring-boot/tree/master/spring-boot-samples/spring-boot-sample-web-ui
--- client-app/src/main/groovy/com/rdas/controller/HomeController.groovy
+++ client-app/src/main/groovy/com/rdas/controller/HomeController.groovy
@@ -1,16 +1,31 @@
 package com.rdas.controller
 
+import org.springframework.http.HttpHeaders
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.RequestHeader
 
 /**
  * Created by rdas on 04/07/2016.
  */
 import org.springframework.web.bind.annotation.RequestMapping
 
+import javax.servlet.http.HttpServletResponse
+
 @Controller
 public class HomeController {
     @RequestMapping("/")
-    public String home() {
+    public String home(@RequestHeader(value = "Accept") String accept,
+                       @RequestHeader(value = "Accept-Language") String acceptLanguage,
+                       @RequestHeader(value = "User-Agent", defaultValue = "foo") String userAgent,
+                       @RequestHeader(value = "sm_user", defaultValue = "sm_u") String smUser,
+                       @RequestHeader HttpHeaders headers,
+                       HttpServletResponse response) {
+        System.out.println("accept: " + accept);
+        System.out.println("acceptLanguage: " + acceptLanguage);
+        System.out.println("userAgent: " + userAgent);
+        System.out.println("smUser: " + smUser);
+
+
         return "index"
     }
 }
